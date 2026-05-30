8e2856c24bd3d9b3a27695f084a342f04d569fc2
Deepak Jangra <Deepakjangra605@gmail.com>
2026-05-30 23:04:44 +0530

    Commit Includes: - Implementation starts for audio player

---
+++ shared/src/iosMain/kotlin/com/deepak/game/domain/AudioPlayer.ios.kt
@@ -0,0 +1,24 @@
+package com.deepak.game.domain
+
+actual class AudioPlayer {
+    actual fun playGameOverSound() {
+    }
+
+    actual fun playJumpSound() {
+    }
+
+    actual fun playFallingSound() {
+    }
+
+    actual fun stopFallingSound() {
+    }
+
+    actual fun playGameSoundInLoop() {
+    }
+
+    actual fun stopGameSound() {
+    }
+
+    actual fun release() {
+    }
+}
